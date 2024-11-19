b5f4c473809b48fb4fc5cd461b411185989ddb0d
BenitesAnderson <tu-email@dominio.com>
2024-11-18 23:23:09 -0500

    segundo

--- ElBarbasShop   01_12  05_11_2024/ElBarbasShop/ElBarbasShop/ElBarbasShop/ElBarbasShop/FrmHorario.Designer.cs
+++ ElBarbasShop   01_12  05_11_2024/ElBarbasShop/ElBarbasShop/ElBarbasShop/ElBarbasShop/FrmHorario.Designer.cs
@@ -109,7 +109,7 @@
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(222, 392);
+            this.button1.Location = new System.Drawing.Point(223, 430);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 30);
             this.button1.TabIndex = 23;
